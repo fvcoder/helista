9b032ac1b3281bf37d5e9d6134a9d81b3c97187a
Fernando Ticona <46249834+fvcoder@users.noreply.github.com>
2023-03-09 20:51:35 +0000

    eslint & css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+	extends: [
+		"@remix-run/eslint-config",
+		"@remix-run/eslint-config/node",
+		"eslint-config-codely/typescript",
+	],
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			parserOptions: {
+				project: ["./tsconfig.json"],
+			},
+		},
+	],
 };
--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,49 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
 } from "@remix-run/react";
 
+import { getCssText } from "./lib/ui";
+
+interface rootLoader {
+	styles: string;
+}
+
+export const loader: LoaderFunction = () => {
+	return json<rootLoader>({
+		styles: getCssText(),
+	});
+};
+
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+	charset: "utf-8",
+	title: "Helista",
+	viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	const { styles } = useLoaderData<rootLoader>();
+
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+				<style id="css">{styles}</style>
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
